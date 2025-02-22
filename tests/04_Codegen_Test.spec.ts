872eae393d3d238767702cadce1e0af653c3df5a
mukul jha <mukuljha302@gmail.com>
2025-02-22 21:15:48 +0530

    5 th commit

--- tests/04_Codegen_Test.spec.ts
+++ tests/04_Codegen_Test.spec.ts
@@ -19,5 +19,5 @@ test('Codegen test case', { tag: ['@PlaywrightWithJenkins'] }, async ({ page }) 
  */
 test('Test 2 will fail', { tag: ['@PlaywrightWithJenkins'] }, async ({ page }) => {
   await page.goto('https://www.youtube.com/@testerstalk');
-  expect(true).toBe(false);
+  expect(true).toBe(true);
 });
